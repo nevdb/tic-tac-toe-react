276e68d1e04b39b9c784956f733bb0a4074412b6
nevena <dnevena27@gmail.com>
2025-12-19 16:47:48 +0200

    Multi-dimensional lists, lifting state up

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import Player from './components/Player';
 import Review from './components/Review';
 import './App.css';
 import { useState } from "react";
+import GameBoard from './components/GameBoard';
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  }
+
   const [feedback, setFeedback] = useState('My feedback');
   const [studentName, setStudentName] = useState('Student Name');
 
@@ -18,12 +25,14 @@ function App() {
   return (
     <main>
       <div id='game-container'>
-        <ol id='players'>
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id='players' className='highligh-player'>
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
         GAME BOARD
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
+
       <div id='feedback-container'>
         <section id="feedback">
           <h2>Please share some feedback</h2>
